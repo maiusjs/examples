8433f1a71d54a2b8982d4f5bce9cfa707ac853be
yleo77 <ylep77@gmail.com>
2018-07-20 16:41:48 +0800

    feat: update `hello world` example

--- helloworld/controller/home.js
+++ helloworld/controller/home.js
@@ -1,10 +1,6 @@
 const { Controller } = require('maius');
 
 module.exports = class HomeController extends Controller {
-  constructor() {
-    super();
-    this.index = this.index.bind(this);
-  }
 
   async index(ctx, next) {
     const str = await this.service.home.say('coolboy');
